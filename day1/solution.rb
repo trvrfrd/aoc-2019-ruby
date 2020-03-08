3655621000487e7511ec24835d47e84c91027a3b
Trevor Ford <info@trvrfrd.net>
2020-03-07 22:13:01 -0500

    make day 1 solution a little clearer

--- day1/solution.rb
+++ day1/solution.rb
@@ -1,29 +1,29 @@
 input_path = File.expand_path("input.txt", File.dirname(__FILE__))
 input = File.readlines(input_path)
 
-def fuel_required(mass)
+def base_fuel_requirement(mass)
   (mass / 3.0).floor - 2
 end
 
 masses = input.map(&:to_i)
-total_fuel_requirement = masses.map { |m| fuel_required(m) }.sum
+fuel_requirement = masses.map { |m| base_fuel_requirement(m) }.sum
 
 # 3331523
-puts total_fuel_requirement
+puts fuel_requirement
 
 # --- Part Two ---
 
-def total_fuel_required(mass)
-  total_fuel_requirement = initial_fuel_requirement = fuel_required(mass)
-  added_fuel_requirement = fuel_required(initial_fuel_requirement)
+def total_fuel_requirement(mass)
+  total_fuel = 0
+  added_fuel = base_fuel_requirement(mass)
 
-  while added_fuel_requirement > 0
-    total_fuel_requirement += added_fuel_requirement
-    added_fuel_requirement = fuel_required(added_fuel_requirement)
+  while added_fuel > 0
+    total_fuel += added_fuel
+    added_fuel = base_fuel_requirement(added_fuel)
   end
 
-  total_fuel_requirement
+  total_fuel
 end
 
 # 4994396
-puts masses.map { |m| total_fuel_required(m) }.sum
+puts masses.map { |m| total_fuel_requirement(m) }.sum
